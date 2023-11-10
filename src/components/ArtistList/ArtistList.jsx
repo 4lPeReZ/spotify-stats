6b3d78a11c31bfc79b026540c9c2cbd8c8690475
4lPeReZ <alvaropr05@hotmail.com>
2023-11-10 13:50:53 +0100

    Add styling and artist images to ArtistList component

--- src/components/ArtistList/ArtistList.jsx
+++ src/components/ArtistList/ArtistList.jsx
@@ -1,14 +1,24 @@
 // src/components/ArtistList.jsx
 import React from 'react';
-import './ArtistList.css';
+import './ArtistList.css'; // Asegúrate de tener tus estilos CSS
 
 const ArtistList = ({ artists }) => {
   return (
-    <div>
+    <div className="artist-list">
       <h1>Top Artists</h1>
       <ul className="no-bullets-artist">
         {artists.map((artist, index) => (
-          <li key={index}>{artist.name}</li>
+          <li key={index} className="artist-item">
+            <span className="artist-position">{index + 1}.</span>
+            {artist.images && artist.images[0] && (
+              <img
+                src={artist.images[0].url}
+                alt={artist.name}
+                className="artist-image"
+              />
+            )}
+            <span className="artist-name">{artist.name}</span>
+          </li>
         ))}
       </ul>
     </div>
